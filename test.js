1b4119127d40c3f38a32cbe243521a9e8fb43a5e
EstherA9 <estera.mircea@gmail.com>
2024-05-03 13:35:06 +0300

    modify

--- test.js
+++
@@ -1,9 +0,0 @@
-let MyName="Estera";
-MyName="Cristi";
-const age = 25;
-
-console.log("Salutare"+MyName);
-console.log("Salutare"+MyName);
-console.log("Salutare"+MyName);
-console.log("Salutare"+MyName);
-console.log("Salutare"+MyName);
