60369a3f2116b3f8bdfbd24b1fd393d1b873f8d8
EstherA9 <estera.mircea@gmail.com>
2024-05-03 13:33:48 +0300

    merge to be done

---
+++ curs_4/test.js
@@ -0,0 +1,9 @@
+let MyName="Estera";
+MyName="Cristi";
+const age = 25;
+
+console.log("Salutare"+MyName);
+console.log("Salutare"+MyName);
+console.log("Salutare"+MyName);
+console.log("Salutare"+MyName);
+console.log("Salutare"+MyName);
