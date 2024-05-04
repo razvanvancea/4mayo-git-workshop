df29d8234e9823267623addaa0c7f0596271734c
EstherA9 <estera.mircea@gmail.com>
2024-05-04 12:41:05 +0300

    adding new work

---
+++ curs_5/node-demo/test.js
@@ -0,0 +1,11 @@
+describe ("Ath test suite", () => {
+    it ("test 1", () => {
+        console.log ('test1')
+    })
+    it ("test 2", () => {
+        console.log ('test2')
+    })
+    it ("test 3", () => {
+        console.log ('test3')
+    })
+})
