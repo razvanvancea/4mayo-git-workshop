9d42ee1219c74910215d8f0715d8ccab032f4944
EstherA9 <estera.mircea@gmail.com>
2024-05-04 17:26:30 +0300

    new commits

--- curs_5/node-demo/test.js
+++ curs_5/node-demo/test.js
@@ -1,6 +1,15 @@
-describe ("Ath test suite", () => {
+describe ("Ath test suite before each", () => {
     beforeEach (() => {
-    console.log ("authentification");
+    console.log ("this a ath before each suite");
+    })
+    before ("Ath test suite before all", ()=>{
+        console.log("this is an ath before all suite");
+    })
+    afterEach("Ath test suite after each", ()=> {
+        console.log("this a after ath on each suite");
+    })
+    after ("Ath test suite after all", ()=>{
+        console.log("this a ath on all suite");
     })
     it ("test 1", () => {
         console.log ('test1')
