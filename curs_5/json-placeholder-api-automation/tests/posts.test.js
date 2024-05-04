10df34c6280da381340d58222dd630c324cc2855
EstherA9 <estera.mircea@gmail.com>
2024-05-04 18:54:43 +0300

    add new thinkgs

--- curs_5/json-placeholder-api-automation/tests/posts.test.js
+++ curs_5/json-placeholder-api-automation/tests/posts.test.js
@@ -1,9 +1,24 @@
-const { spec } = require('pactum');
+const { spec, request } = require('pactum');
+const baseURL = 'https://jsonplaceholder.typicode.com';
 
 describe ("POST test suite", () =>{
+    before( async ()=>{
+        request.setDefaultTimeout(10000);
+    });
+
     it ("GET posts test", async () =>{
        await spec()
-       .get("https://jsonplaceholder.typicode.com/posts")
+       .get(`${baseURL}/posts`)
        .expectStatus(200);
+    });
+    
+    it ("create test", async () => {
+        const requestBody = {
+            "title":"ceva",
+            "body":"altceva"
+        }
+        await spec().post(`${baseURL}/posts`)
+        .withBody(`${requestBody}`)
+        .expectStatus(201);
     });
 });
